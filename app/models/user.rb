46fe053c6823a2986a88896b7c96d8dd95a08a64
navyarani-k <navyarani.k@etvbharat.com>
2020-02-27 17:18:26 +0530

    username and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+ # validates :name, presence: true
+ validates :name, presence: true ,length: { maximum: 50}
+ validates :email, presence: true, length: {maximum: 100 }
+  # validates :email, format: { with: /<regular expression>/ }
 end
